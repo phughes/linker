a78cfee25887189854ae22938cc5632448e16b24
Patrick Hughes <pat@phughes.us>
2018-09-12 15:03:05 -0400

    Initial Commit

---
+++ LinkOpener/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkOpener
+//
+//  Created by Patrick Hughes on 9/12/18.
+//  Copyright © 2018 Patrick Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
